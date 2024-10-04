923b2cf979e6898246fe0350a2529a3e591a3b0f
kidist demessie <kidideme@gmail.com>
2024-10-04 17:08:14 +0300

    outliers are done according to the data type.

--- Scripts/EDA.py
+++ Scripts/EDA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
-
+import numpy as np
 class EDA:
     def __init__(self, file_path):
         self.data = pd.read_csv(file_path)
@@ -29,44 +29,60 @@ class EDA:
 
         return numerical_summary, categorical_summary
 
+ 
+
     def distribution_of_numerical_features(self):
-        numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
-        print(f"Numerical Columns: {numerical_cols}")
+        numerical_cols = ['CountryCode', 'Amount', 'Value', 'PricingStrategy', 'FraudResult']
         
-        n = len(numerical_cols)
-        rows = (n + 1) // 2
-        fig, axes = plt.subplots(nrows=rows, ncols=2, figsize=(12, 5 * rows))
-        fig.suptitle('Distribution of Numerical Features', fontsize=16)
+        # Create subplots with 2 rows and 3 columns
+        num_plots = len(numerical_cols)
+        fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(18, 10), squeeze=False)  # Adjust size as needed
+        
+        # Flatten the axs array for easy iteration
+        axs = axs.flatten()
+        
+        for ax, col in zip(axs, numerical_cols):
+            if col == 'CountryCode':
+                # For categorical-like numerical features, you can use a count plot
+                sns.countplot(data=self.data, x=col, palette='pastel', ax=ax)
+                ax.set_title(f'Distribution of {col}', fontsize=16)
+                ax.set_xlabel(col, fontsize=12)
+                ax.set_ylabel('Count', fontsize=12)
+                ax.tick_params(axis='x', rotation=45)
+            else:
+                # For other numerical features, use a histogram
+                sns.histplot(data=self.data, x=col, bins=30, kde=True, ax=ax, color='skyblue')
+                ax.set_title(f'Distribution of {col}', fontsize=16)
+                ax.set_xlabel(col, fontsize=12)
+                ax.set_ylabel('Frequency', fontsize=12)
 
-        for i, col in enumerate(numerical_cols):
-            row, col_idx = divmod(i, 2)
-            sns.histplot(self.data[col], kde=True, ax=axes[row, col_idx], color='teal', bins=30)
-            axes[row, col_idx].set_title(f'Distribution of {col}', fontsize=14)
-            axes[row, col_idx].set_xlabel(col, fontsize=12)
-            axes[row, col_idx].set_ylabel('Frequency', fontsize=12)
-            axes[row, col_idx].grid(True)
+            ax.grid(True)  # Add gridlines
 
-        if n % 2 != 0:
-            fig.delaxes(axes[rows - 1, 1])
-
-        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.savefig('../Images/distribution_of_numerical_features.png')
+        # Hide any unused subplots if there are less than 6 plots
+        for i in range(num_plots, len(axs)):
+            fig.delaxes(axs.flatten()[i])
+        
+        plt.tight_layout()
+        plt.savefig("../Images/distribution_of_numerical_features.png")  # Save all subplots as one image
         plt.show()
-        plt.close(fig)
 
     def distribution_of_categorical_features(self):
         meaningful_categorical_cols = ['CurrencyCode', 'ProductCategory', 'ChannelId']
         
-        for col in meaningful_categorical_cols:
-            plt.figure(figsize=(10, 6))
-            sns.countplot(data=self.data, x=col, palette='pastel')
-            plt.title(f'Distribution of {col}', fontsize=16)
-            plt.xlabel(col, fontsize=12)
-            plt.ylabel('Count', fontsize=12)
-            plt.xticks(rotation=45)
-            plt.tight_layout()
-            plt.savefig(f"../Images/distribution_of_{col}.png")
-            plt.show()
+        # Create subplots
+        fig, axs = plt.subplots(nrows=1, ncols=len(meaningful_categorical_cols), figsize=(18, 6))
+        
+        for ax, col in zip(axs, meaningful_categorical_cols):
+            sns.countplot(data=self.data, x=col, palette='pastel', ax=ax)
+            ax.set_title(f'Distribution of {col}', fontsize=16)
+            ax.set_xlabel(col, fontsize=12)
+            ax.set_ylabel('Count', fontsize=12)
+            ax.tick_params(axis='x', rotation=45)
+            ax.grid(True)  # Add gridlines
+        
+        plt.tight_layout()
+        plt.savefig("../Images/distribution_of_categorical_features.png")  # Save all subplots as one image
+        plt.show()
 
     def correlation_analysis(self):
         numeric_data = self.data.select_dtypes(include=['float64', 'int64'])
@@ -126,48 +142,86 @@ class EDA:
             plt.show()
         else:
             print("No missing values in the dataset.")
-
-    def detect_outliers(self, method='IQR'):
-        numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
-        outliers_dict = {}
-
-        for col in numerical_cols:
-            if method == 'IQR':
-                Q1 = self.data[col].quantile(0.25)
-                Q3 = self.data[col].quantile(0.75)
-                IQR = Q3 - Q1
-                lower_bound = Q1 - 1.5 * IQR
-                upper_bound = Q3 + 1.5 * IQR
-                outliers = self.data[(self.data[col] < lower_bound) | (self.data[col] > upper_bound)]
-                outliers_dict[col] = outliers
-                print(f"{len(outliers)} outliers detected in {col} using IQR method.")
-        
-        return outliers_dict
-
-    def handle_outliers(self, method='remove'):
-        if method not in ['remove', 'replace']:
-            raise ValueError("Method must be 'remove' or 'replace'.")
-
-        numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
-        
-        for col in numerical_cols:
-            Q1 = self.data[col].quantile(0.25)
-            Q3 = self.data[col].quantile(0.75)
-            IQR = Q3 - Q1
-            lower_bound = Q1 - 1.5 * IQR
-            upper_bound = Q3 + 1.5 * IQR
+    def detect_outliers(self, method='IQR'): 
+            numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
+            outliers_dict = {}
             
-            if method == 'remove':
-                initial_count = self.data.shape[0]
-                self.data = self.data[(self.data[col] >= lower_bound) & (self.data[col] <= upper_bound)]
-                final_count = self.data.shape[0]
-                print(f"Removed {initial_count - final_count} outliers from {col}.")
-            elif method == 'replace':
-                self.data[col] = self.data[col].mask(self.data[col] < lower_bound, lower_bound)
-                self.data[col] = self.data[col].mask(self.data[col] > upper_bound, upper_bound)
-                print(f"Replaced outliers in {col} with bounds.")
+            # Create subplots for box plots
+            num_plots = len(numerical_cols)
+            fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(18, 10), squeeze=False)  # Adjust size as needed
+            axs = axs.flatten()  # Flatten the array for easy iteration
 
-        # Save cleaned data
-        cleaned_file_path = '../Data/cleaned_data.csv'
-        self.data.to_csv(cleaned_file_path, index=False)
-        print(f"Cleaned data saved to {cleaned_file_path}.")
+            for idx, col in enumerate(numerical_cols):
+                if method == 'IQR':
+                    Q1 = self.data[col].quantile(0.25)
+                    Q3 = self.data[col].quantile(0.75)
+                    IQR = Q3 - Q1
+                    lower_bound = Q1 - 1.5 * IQR
+                    upper_bound = Q3 + 1.5 * IQR
+                    
+                    # Detect outliers
+                    outliers = self.data[(self.data[col] < lower_bound) | (self.data[col] > upper_bound)]
+                    outliers_dict[col] = outliers
+                    
+                    # Calculate percentage of outliers
+                    total_points = len(self.data[col])
+                    num_outliers = len(outliers)
+                    percent_outliers = (num_outliers / total_points) * 100
+                    
+                    print(f"{num_outliers} outliers detected in {col} using IQR method. ({percent_outliers:.2f}% of data)")
+                
+                # Create a box plot for the current column
+                sns.boxplot(data=self.data, y=col, ax=axs[idx])
+                axs[idx].set_title(f'Box Plot of {col}', fontsize=16)
+                axs[idx].set_ylabel(col, fontsize=12)
+                axs[idx].grid(True)  # Add gridlines
+
+            # Hide any unused subplots if there are less than 6 plots
+            for i in range(num_plots, len(axs)):
+                fig.delaxes(axs.flatten()[i])
+            
+            plt.tight_layout()
+            plt.savefig("../Images/outlier_detection_box_plots.png")  # Save all subplots as one image
+            plt.show()
+            
+            return outliers_dict
+
+   
+
+    def handle_outliers(self, save_path="../Data/clean_data.csv"):
+        numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
+        
+        for col in numerical_cols:
+            if col == 'Amount':
+                # Log transformation for Amount to reduce skew
+                print(f"Applying log transformation to {col}")
+                self.data[col] = np.log1p(self.data[col])  # log1p to handle zeros as well
+                
+            elif col == 'Value':
+                # Winsorization for Value (cap extreme values)
+                print(f"Applying Winsorization to {col}")
+                lower_percentile = self.data[col].quantile(0.05)
+                upper_percentile = self.data[col].quantile(0.95)
+                self.data[col] = np.clip(self.data[col], lower_percentile, upper_percentile)
+            
+            elif col == 'PricingStrategy':
+                # Binning PricingStrategy to group outliers into categories
+                print(f"Applying binning to {col}")
+                self.data[col] = pd.cut(self.data[col], bins=[-np.inf, 1, 2, np.inf], labels=['Low', 'Medium', 'High'])
+            
+            elif col == 'FraudResult':
+                # No outlier removal for FraudResult, retain as-is
+                print(f"Retaining outliers in {col}, as they are crucial for analysis")
+            
+            else:
+                # Default method if needed for other columns
+                print(f"Handling outliers for {col} using default method (Winsorization)")
+                lower_percentile = self.data[col].quantile(0.05)
+                upper_percentile = self.data[col].quantile(0.95)
+                self.data[col] = np.clip(self.data[col], lower_percentile, upper_percentile)
+
+        # Save the cleaned data to a CSV file
+        self.data.to_csv(save_path, index=False)
+        print(f"Cleaned data saved to {save_path}")
+        
+        return self.data
